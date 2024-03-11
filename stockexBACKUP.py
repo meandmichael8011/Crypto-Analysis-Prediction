d216a613f7830354096eaffd38cc93d850e42222
meandmichael8011 <136816748+meandmichael8011@users.noreply.github.com>
2024-03-11 23:28:38 +0500

    Update stockexBACKUP.py

--- stockexBACKUP.py
+++ stockexBACKUP.py
@@ -17,22 +17,6 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 import yfinance as yf
-
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import pandas_datareader as web
-import datetime as dt
-import yfinance as yf
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import pandas_datareader as web
-import datetime as dt
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.layers import Dense, Dropout, LSTM
-from tensorflow.keras.models import Sequential
-import yfinance as yf
 import streamlit as st
 
 
